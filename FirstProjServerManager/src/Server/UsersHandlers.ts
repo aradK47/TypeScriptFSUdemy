cafc499fcb149d5463cdb3c939e91795f45c47fe
thyself <aradka123@gmail.com>
2020-07-28 05:02:03 +0300

    few more changes made

--- FirstProjServerManager/src/Server/UsersHandlers.ts
+++ FirstProjServerManager/src/Server/UsersHandlers.ts
@@ -4,6 +4,7 @@ import { UserDBAccess } from "../User/UsersDBAccess";
 import { HTTP_METHODS, HTTP_CODES, AccessRight, User } from "../Shared/Model";
 import { Utils } from "./Utils";
 import { BaseRequestHandler } from "./BaseReqestHandler";
+import { utimes } from "fs";
 
 export class UsersHandler extends BaseRequestHandler {
 
@@ -22,6 +23,9 @@ export class UsersHandler extends BaseRequestHandler {
                 break;
             case HTTP_METHODS.PUT:
                 await this.handlePut();
+                break;
+            case HTTP_METHODS.DELETE:
+                await this.handleDelete();
                 break;
             default:
                 this.handleNotFound();
@@ -45,22 +49,46 @@ export class UsersHandler extends BaseRequestHandler {
 
     }
 
+    private async handleDelete(){
+        const operationAuthorized = await this.operationAuthorized(AccessRight.DELETE);
+        if (operationAuthorized) {
+            const parsedUrlUserId = Utils.getUrlParameters(this.req.url);
+            if (parsedUrlUserId) {
+                if (parsedUrlUserId?.query.id) {
+                    const deleteResult = await  this.userDBAccess.deleteUser(parsedUrlUserId?.query.id as string)
+                    if (deleteResult) {
+                        this.respondText(HTTP_CODES.OK, `user: ${parsedUrlUserId?.query.id} is deleted.`)
+                    } else {
+                        this.respondText(HTTP_CODES.NOT_FOUND, `user: ${parsedUrlUserId?.query.id} was not deleted. (not found)`)
+                    }
+                } else {
+                    this.respondBadRequest('missing id in the request')
+                }
+            } else {
+                
+            }
+        }
+    }
+
 
     private async handleGet() {
         const operationAuthorized = await this.operationAuthorized(AccessRight.READ)
         if (operationAuthorized) {
             const parsedUrl = Utils.getUrlParameters(this.req.url);
             if (parsedUrl) {
-                const userId = parsedUrl?.query.id;
-                if (userId) {
-                    const user = await this.userDBAccess.getUserById(userId as string);
+                if (parsedUrl?.query.id) {
+                    const user = await this.userDBAccess.getUserById(parsedUrl?.query.id as string);
                     if (user) {
                         this.respondJsonObject(HTTP_CODES.OK, user)
                     } else {
                         this.handleNotFound()
                     }
+                } else if (parsedUrl?.query.name) {
+                    console.log("shit is shit")
+                    const users = await this.userDBAccess.getUserByName(parsedUrl?.query.name as string)
+                    this.respondJsonObject(HTTP_CODES.OK, users)
                 } else {
-                    this.respondBadRequest("userId not present in request")
+                    this.respondBadRequest("userId or name not present in request")
                 }
             }
         } else {
--- FirstProjServerManager/src/User/UsersDBAccess.ts
+++ FirstProjServerManager/src/User/UsersDBAccess.ts
@@ -24,13 +24,13 @@ export class UserDBAccess {
         })
     }
 
-    public async getUserById(userId: string): Promise<User | undefined> {        
+    public async getUserById(userId: string): Promise<User | undefined> {
         return new Promise((resolve, reject) => {
-            this.nedb.find({ id: userId }, (err: Error, docs: any[])=>{
+            this.nedb.find({ id: userId }, (err: Error, docs: any[]) => {
                 if (err) {
                     reject(err)
                 } else {
-                    if(docs.length == 0) {
+                    if (docs.length == 0) {
                         resolve(undefined)
                     } else {
                         resolve(docs[0])
@@ -40,7 +40,42 @@ export class UserDBAccess {
         })
     }
 
-    private generateUserId(){
+    public async deleteUser(userId: string): Promise<boolean> {
+        const operationSuccess = await this.deleteUserFromDB(userId);
+        this.nedb.loadDatabase()
+        return operationSuccess;
+    }
+
+    private async deleteUserFromDB(userId: string): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            this.nedb.remove({ id: userId }, (err: Error | null, numRemoved: number) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    if (numRemoved == 0) {
+                        resolve(false)
+                    } else {
+                        resolve(true)
+                    }
+                }
+            })
+        })
+    }
+
+    public async getUserByName(name: string): Promise<User[]> {
+        const regEx = new RegExp(name)
+        return new Promise((resolve, reject) => {
+            this.nedb.find({ name: regEx }, (err: Error, docs: any[]) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(docs)
+                }
+            })
+        })
+    }
+
+    private generateUserId() {
         return Math.random().toString(36).slice(2);
     }
 }
